74e229f56c2391d7e7b9c0b0f21dde22ee0a79f2
Karim Mehour <kmehour@c1r8p2.42quebec.com>
2025-01-09 22:58:30 -0500

    Add ex03

---
+++ 0-python/ex03/NULL_not_found.py
@@ -0,0 +1,19 @@
+Map = {
+	type(None): "Nothing",
+	float: "Cheese",
+	int: "Zero",
+	str: "Empty",
+	bool: "Fake"
+}
+
+def NULL_not_found(object: any) -> int:
+	obj_type = type(object)
+
+	if object != object:
+		pass
+	elif object or obj_type not in Map:
+		print("Type not found")
+		return 1
+	
+	print(Map[obj_type], ":", object, type(object))
+	return 0
